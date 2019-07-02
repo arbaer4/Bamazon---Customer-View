094789af3988d419c47c34fdc8ef095eeea81374
Alexandra Baer <alexbaer@Alexandras-MacBook-Pro.local>
2019-07-02 12:33:25 -0400

    updated to include insufficient quantity and subtract request from total

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -76,8 +76,6 @@ function afterConnection() {
 
         //take product ID they entered and check to make sure there are enough in stock to sell them
 
-        //If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
         //if your store _does_ have enough of the product, you should fulfill the customer's order.
         //This means updating the SQL database to reflect the remaining quantity.
         //Once the update goes through, show the customer the total cost of their purchase.
@@ -86,49 +84,50 @@ function afterConnection() {
         .then(function (answer) {
           console.log(answer);
           // find out how many items we have of particular item_id
-          connection.query("SELECT stock_quantity FROM products where item_id=?", [answer.choice], function(err, res) {
+          connection.query("SELECT stock_quantity FROM products where item_id=?", [answer.choice], function (err, res) {
             console.log(res[0].stock_quantity);
             console.log(answer.quantity);
-              if (answer.quantity <= res[0].stock_quantity) {
-                //update db, let user know and start over
-                console.log("we have enough");
-
-               
-              }
-              //else alert user of insufficient supply 
-
-              else {
-                // bid wasn't high enough, so apologize and start over
-                console.log("Insufficient quantity!");
-                connection.end();
-              }
+            if (answer.quantity <= res[0].stock_quantity) {
+              //update db, let user know and start over
+              console.log("we have enough");
+              var newStock = res[0].stock_quantity - answer.quantity;
+              var userChoice = answer.choice;
+              // console.log(newStock);
+              //this is where we will call new function and pass in new stock
+              updateProducts(newStock, userChoice);
+            }
+            else {
+              // bid wasn't high enough, so apologize and start over
+              console.log("Insufficient quantity!");
+              stop();
+            }
           });
 
-          // // determine if enough in stock
-          // if (false) {
-          //   // stock was high enough, so update db, let the user know, and start over
-          //   connection.query(
-          //     "UPDATE products SET ? WHERE ?",
-          //     [
-          //       {
-          //         stock_quantity: answer.quantity
-          //       },
-          //       {
-          //         item_id: answer.choice
-          //       }
-          //     ],
-          //     function (error) {
-          //       if (error) throw err;
-          //       console.log("Bid placed successfully!");
-          //     }
-          //   );
-          // }
-          // else {
-          //   // bid wasn't high enough, so apologize and start over
-          //   console.log("Insufficient quantity!");
-          //   connection.end();
-          // }
-        });
+
+          function updateProducts(newStock, userChoice) {
+            // console.log("this is the total new stock: " + newStock)
+            // console.log("This was the user ID selected: " + userChoice)
+            connection.query(
+              "UPDATE products SET stock_quantity=? WHERE item_id=?",
+              [newStock, userChoice],
+
+              function (error) {
+                if (error) throw err;
+                console.log("Products have been purchased!");
+                stop();
+              }
+            );
+          }
+
+          function stop() {
+            connection.end()
+          }
+
+
+
+
+
+        })
     }
   })
 }
